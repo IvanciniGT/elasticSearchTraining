fc1bfc52b6dc5725f1cbd95dbdb361a3c9e9f48e
Ubuntu <ubuntu@ip-172-31-3-117.eu-west-1.compute.internal>
2022-11-22 19:56:49 +0000

    day10

--- examples/beats/heartbeat/start.sh
+++ examples/beats/heartbeat/start.sh
@@ -3,6 +3,7 @@ docker rm myheartbeat -f && echo deleted || echo not found
 docker container create \
     -v $PWD/apache.heartbeat.yaml:/usr/share/heartbeat/heartbeat.yml:ro \
     --name myheartbeat \
-    elastic/heartbeat:8.5.0
+    elastic/heartbeat:8.5.0 \
+    --strict.perms=false 
     
 docker container start myheartbeat
--- examples/logstash/start.sh
+++ examples/logstash/start.sh
@@ -3,6 +3,8 @@ docker run \
     -it \
     --rm \
     -p 5044:5044 \
+    -p 5045:5045 \
+    -p 5046:5046 \
     -v /home/ubuntu/environment/training/examples/logstash/data:/data \
     -v /home/ubuntu/environment/training/examples/logstash/templates:/templates \
     -v /home/ubuntu/environment/training/examples/logstash/pipelines/$1:/usr/share/logstash/pipeline/logstash.conf \
